924a0ca046482c270368f3c06cf4563df393dde7
arshad012 <forarshad99@gmail.com>
2024-10-21 19:22:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+
 //your JS code here. If required.
+function open_modal() {
+	const modal_content = document.querySelector('.modal-content');
+	modal_content.style.top = 0;
+}
+
+function close_modal() {
+	const modal_content = document.querySelector('.modal-content');
+	modal_content.style.top = '-150px';
+}
