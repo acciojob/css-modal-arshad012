069942f1ef0e6b64a9395e020bacb66ea1ed176d
arshad012 <forarshad99@gmail.com>
2024-10-21 19:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 //your JS code here. If required.
 function open_modal() {
+	const modal = document.querySelector('.modal');
+	modal.style.display = 'block';
 	const modal_content = document.querySelector('.modal-content');
 	modal_content.style.top = 0;
 }
 
 function close_modal() {
+	const modal = document.querySelector('.modal');
+	modal.style.display = 'none';
 	const modal_content = document.querySelector('.modal-content');
 	modal_content.style.top = '-150px';
 }
