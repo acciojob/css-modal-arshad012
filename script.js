1531b5790f5df8a963ec85b203e3676c9ad9b319
arshad012 <forarshad99@gmail.com>
2024-10-21 19:39:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 function open_modal() {
 	const modal = document.querySelector('.modal');
 	modal.style.display = 'block';
-	const modal_content = document.querySelector('.modal-content');
-	modal_content.style.top = 0;
+	// const modal_content = document.querySelector('.modal-content');
+	// modal_content.style.top = 0;
 }
 
 function close_modal() {
 	const modal = document.querySelector('.modal');
 	modal.style.display = 'none';
-	const modal_content = document.querySelector('.modal-content');
-	modal_content.style.top = '-150px';
+	// const modal_content = document.querySelector('.modal-content');
+	// modal_content.style.top = '-150px';
 }
 
 
